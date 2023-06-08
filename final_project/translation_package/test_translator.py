12971df4e3bc115f7953ea772bcdfcc157886fbc
Nwiradiradja <NateWiradiradja@gmail.com>
2023-06-07 21:36:59 -0400

    Updated Project

--- final_project/translation_package/test_translator.py
+++ final_project/translation_package/test_translator.py
@@ -5,10 +5,13 @@ class TranslationTests(unittest.TestCase):
     def test_english_to_french(self):
         translation = english_to_french("hello")
         self.assertEqual(translation, "bonjour")
+        self.assertNotEqual(translation, "hola")
 
     def test_french_to_english(self):
         translation = french_to_english("bonjour")
         self.assertEqual(translation, "hello")
+        self.assertNotEqual(translation, "hola")
+
 
 if __name__ == "__main__":
     unittest.main()
--- final_project/translation_package/translator.py
+++ final_project/translation_package/translator.py
@@ -8,6 +8,7 @@ def english_to_french(text):
     translation = fr_translator.translate(text)
     return translation
 
+
 def french_to_english(text):
     translation = en_translator.translate(text)
     return translation
